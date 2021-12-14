377da89a1fe88aeb3c9c7d70d379e1d938718339
Stefani Arabadjieva <s.arabadjieva00@gmail.com>
2021-12-14 18:01:56 +0200

    Initial commit

---
+++ LOTRAkinator/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOTRAkinator
+{
+    class GameController
+    {
+        private bool isFinished;
+
+        private List<string> questions;
+        private List<string> characters;
+
+        public bool IsFinished
+        {
+            get => isFinished;
+        }
+
+        public GameController(string languageSetting)
+        {
+            SetLanguageSetting(languageSetting);
+        }
+
+
+        private void SetLanguageSetting(string languageSetting)
+        {
+            switch (languageSetting.ToLower())
+            {
+                case "en":
+                case "english":
+                    //load english files
+                    break;
+                case "bg":
+                case "bulgarian":
+                    //load bulgarian files
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void AskQuestion()
+        {
+
+        }
+
+        private void GetQuestion()
+        {
+            Console.WriteLine(questions[0]);
+            string answer = Console.ReadLine();
+        }
+    }
+}
---
+++ LOTRAkinator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LOTRAkinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please choose language!");
+            string lang = Console.ReadLine();
+
+            GameController game = new GameController(lang);
+
+            //game loop
+            while (!game.IsFinished)
+            {
+                game.AskQuestion();
+            }
+        }
+
+    }
+}
